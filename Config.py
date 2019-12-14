a5800edecbf91e84643a83661ee52160e99fc4c6
ssomanathan <35579317+ssomanathan@users.noreply.github.com>
2019-12-14 17:56:48 +0000

    config change

--- Config.py
+++ Config.py
@@ -2,5 +2,5 @@ mysql={
     'host':"somanathan.mysql.eu.pythonanywhere-services.com",
     'username':'somanathan',
     'password':'somu6675',
-    'database': 'Accidents'
+    'database': 'somanathan$Accidents'
 }
